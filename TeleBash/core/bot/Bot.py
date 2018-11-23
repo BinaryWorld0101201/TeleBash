3e4d6d549639e0d8f153c08fa134adb0042a178b
sadmonad <sadmonad@gmail.com>
2018-11-23 14:00:24 +0300

    Removed current command counter & implemented keyboard layout handler

--- TeleBash/core/bot/Bot.py
+++ TeleBash/core/bot/Bot.py
@@ -1,5 +1,7 @@
 import json
 import subprocess
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.bot import Bot
 from telegram.ext import Updater, CommandHandler
 
@@ -14,30 +16,56 @@ def cmd_runner(command):
     return subprocess.run([command['cmd'], command['args']], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 
-class TeleBashBot(Bot):
+class TeleBashBot:
     def __init__(self, config):
         self.config = {}
         try:
             self.load_config(config)
             self.updater = Updater(self.config['bot_token'])
-            self.current_cmd_number = -1
             self.add_handlers()
         except FileNotFoundError:
             print('File does not exist')
 
+    def get_current_cmd(self, user_text):
+        cmd = list(filter(lambda cmd: cmd['telegram_cmd'] == user_text.replace('/', ''), self.config['bot_commands']))
+
+        if cmd[0]['type'] == 'tg_keyboard':
+            buttons = cmd[0]['buttons']
+            cmd = {'cmd_type': 'keyboard_markup', 'buttons': buttons}
+        else:
+            current_cmd = cmd[0]['bash_cmd']['cmd']
+            current_cmd_args = cmd[0]['bash_cmd']['args']
+            cmd = {'cmd_type': 'bash_command', 'cmd': current_cmd, 'args': ' '.join(current_cmd_args)}
+        return cmd
+
     def add_handlers(self):
         for cmd in self.config['bot_commands']:
-            # TODO: Figure out what to do with this shit
-            self.current_cmd_number += 1
-            self.updater.dispatcher.add_handler(CommandHandler(cmd['telegram_cmd'], self.handler, pass_user_data=True))
+            handler = None
+            pass_user_data = True
+            if cmd['type'] == 'tg_cmd':
+                handler = self.handler
+                pass_user_data = True
+            elif cmd['type'] == 'tg_keyboard':
+                handler = self.keyboard_layout_handler
+                pass_user_data = False
+
+            self.updater.dispatcher.add_handler(CommandHandler(cmd['telegram_cmd'], handler, pass_user_data=pass_user_data))
 
     def handler(self, bot, update, user_data):
-        current_cmd = self.config['bot_commands'][self.current_cmd_number]['bash_cmd']['cmd']
-        current_cmd_args = self.config['bot_commands'][self.current_cmd_number]['bash_cmd']['args']
-        cmd = {'cmd': current_cmd, 'args': ' '.join(current_cmd_args)}
-
+        cmd = self.get_current_cmd(update.message.text)
         user_data['cmd_result'] = cmd_runner(cmd)
         update.message.reply_text(user_data['cmd_result'])
+
+    def keyboard_layout_handler(self, bot, update):
+        cmd = self.get_current_cmd(update.message.text)
+
+        keyboard = [
+            [InlineKeyboardButton(btn['caption'], callback_data='1') for btn in cmd['buttons']]
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        update.message.reply_text('Available commands: ', reply_markup=reply_markup)
 
     def run(self):
         self.updater.start_polling()
